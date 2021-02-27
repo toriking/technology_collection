f9adc30b9f044f483cb006327f68f6fbbec9629c
tomoki <toriiirito@outlook.jp>
2021-02-27 21:24:39 +0900

    days1

---
+++ android/app/src/main/kotlin/com/example/technology_collection/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.technology_collection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // final wordPair = WordPair.random();
+    return MaterialApp(
+      title: 'Technology collection',
+      home: RandomWords(),
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      // home: Scaffold(
+      //   appBar: AppBar(
+      //     title: Text('Technology collection'),
+      //   ),
+      //   body: Center(
+      //     child: RandomWords(),
+      //   ),
+      // ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 8);
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Technology collection'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(26),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+          final int index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
+
+// class RandomWords extends StatefulWidget {
+//   @override
+//   RandomWordsState createState() => RandomWordsSatate();
+// }
+//
+// class _RandomWordsState extends State<RandomWords> {
+//     @override
+//   Widget build(BuildContext content){
+//       final wordPair = WordPair.random();
+//       return Text(wordPair.asPascalCase)
+//   }
+//   }
