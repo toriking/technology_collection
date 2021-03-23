91331b11ea3dfcd0f02f84eea2fa228c962b2f0f
tomoki <toriiirito@outlook.jp>
2021-03-23 23:29:07 +0900

    22days

--- lib/technology_template/drawers.dart
+++ lib/technology_template/drawers.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
+import 'basics_drawer_pages/page1.dart';
+
+// シンプルな書き方
+class Routes {
+  static const String page1 = Page1.routeName;
+}
 
 class Drawers extends StatelessWidget {
-  static const String routeName = '/tete';
+  static const String routeName = '/page1';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(
-        children: [Image.asset('images/flutter_icon.png'), Text('tete')],
-      )),
+      appBar: AppBar(
+        title: Text('標準ドロワー'),
+      ),
+      body: Center(),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              title: Text('ページ1へ'),
+              leading: Icon(Icons.widgets),
+              trailing: Icon(Icons.arrow_back),
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.pushReplacementNamed(context, Routes.page1);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
